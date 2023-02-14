e6446638bae80f8c9aca42bd7e00526f7fc3a34b
Assel Amanzholova <amanzholova-2003@inbox.ru>
2023-02-14 09:11:57 +0600

    lab4.2.py changed

--- lab4.2.py
+++ lab4.2.py
@@ -1,8 +1,8 @@
-x,y = map(str,input().split())
+x = input()
+y = input()
+while not(x.isdigit() and y.isdigit()):
+    x = input()
+    y = input()
+print(int(x) + int(y))
 
-while not (x.isdigit() and y.isdigit()):
-
-   x,y = map(str,input().split())
-
-print(int(x)+int(y))
 
